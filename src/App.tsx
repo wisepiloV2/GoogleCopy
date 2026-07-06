1f4a6834e64ac4715c52bf90b7e4ca030c365830
Wisepilo <maty20022@gmail.com>
2026-07-05 21:52:14 -0300

    Primeros diseños visuales

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import TopBar from "./components/visualDesign/TopBar";
+import BookmarksBar from "./components/visualDesign/BookmarksBar";
+import SearchBar from "./components/visualDesign/SearchBar";
+
+function App() {
+  return (
+      <>
+      <BookmarksBar />
+      <TopBar />
+      <SearchBar />
+      </>
+    );
+}
+
+export default App
